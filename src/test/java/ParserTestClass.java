addd52283bb48fed2f4b911b2787e62a979bd320
Priyanka Narla <priyanka.narla@monsanto.com>
2020-04-27 16:05:29 -0500

    fix test case execution error

--- src/test/java/ParserTestClass.java
+++ src/test/java/ParserTestClass.java
@@ -1,14 +1,11 @@
 import com.parser.exercise.TextFileLogParser;
 import org.junit.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.springframework.util.Assert;
 
-@RunWith(JUnitPlatform.class)
 public class ParserTestClass {
 
     @Test
-     private void testTextFileLogParser(){
+     public void testTextFileLogParser(){
         String logText = " Random Text " +
                 "too look for   a special text ;" +
                 "in a file with tabs and new line characters ";
