59587a7ab7076d05e719b18f031524223e1e36d6
evruc <aaron.matthew.1.smith@monsanto.com>
2020-04-27 12:14:47 -0500

    Update gitignroe

--- src/test/java/ParserTestClass.java
+++ src/test/java/ParserTestClass.java
@@ -1,15 +1,14 @@
 import com.parser.exercise.TextFileLogParser;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.util.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@RunWith(JUnitPlatform.class)
 public class ParserTestClass {
 
     @Test
-     private void testTextFileLogParser(){
+    public void testTextFileLogParser(){
         String logText = " Random Text " +
                 "too look for   a special text ;" +
                 "in a file with tabs and new line characters ";
